4832840e485058f6fa4ea0242cf63a87ac783004
Anish-Karthik <anishkarthik.54321@gmail.com>
2023-05-22 12:26:30 +0530

    Added color changing theme every 2 seconds and some css variables for color

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+
+
 createTiles(320);
+autoChangeColor(2000);
+
+async function sleep(millis) {
+    return new Promise(resolve => setTimeout(resolve, millis));
+}
+
+
+
+// setColor('--primary-color', '#ff0000');
 function createTiles(n) {
     let section = $('section');
     for(let i = 0; i < n; i++) {
@@ -6,4 +17,22 @@ function createTiles(n) {
         // tile.addClass('sp');
         section.append(tile);
     }
+}
+
+function setColor(cssVar, color) {
+    $(':root').css(cssVar, color);
+    // or
+    // document.documentElement.style.setProperty(cssVar, color);
+}
+
+function setRandomColor() {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    setColor('--primary-color', '#'+randomColor);
+}
+
+async function autoChangeColor(millis) {
+    
+    await sleep(millis);
+    setRandomColor();
+    autoChangeColor(millis);
 }
